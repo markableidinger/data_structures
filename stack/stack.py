1868f3d7e18931bfc5bd00451fd1ab63b8115fbc
Mark Ableidinger <mark.ableidinger@gmail.com>
2014-10-21 19:54:35 -0700

    improves stack and adds tests

--- stack/stack.py
+++ stack/stack.py
@@ -13,9 +13,11 @@ class Stack:
     def pop(self):
         '''Removes head item and returns value, makes next item the new head'''
         if self.head.value is not None:
-            final = self.head
-            self.head = final.pointer
-        return final.value
+            return_value = self.head
+            self.head = return_value.pointer
+            return return_value.value
+        else:
+            return None
 
     def peek(self):
         '''Returns value of head item'''
--- stack/test_stack.py
+++ stack/test_stack.py
@@ -1,6 +1,7 @@
 import unittest
 from stack import *
 
+
 class StackTestCase(unittest.TestCase):
     """Tests for `stack.py`."""
 
@@ -10,14 +11,15 @@ class StackTestCase(unittest.TestCase):
         self.assertIsInstance(s, Stack)
 
     def test_push_peek(self):
-        '''Tests that the push function can add an object and the peek function can see it'''
+        '''Tests that the push function can add an
+                object and the peek function can see it'''
         s = Stack()
         s.push(1)
-        self.assertTrue(s.peek()==1)
+        self.assertTrue(s.peek() == 1)
 
     def test_push_pop(self):
         '''Tests the push and pop functions work as expected'''
-        s =  Stack()
+        s = Stack()
         s.push(1)
         s.push(2)
         s.push(3)
@@ -25,7 +27,23 @@ class StackTestCase(unittest.TestCase):
         list.append(s.pop())
         list.append(s.pop())
         list.append(s.pop())
-        self.assertTrue(list == [3,2,1])
+        self.assertTrue(list == [3, 2, 1])
+
+    def test_pop_empty(self):
+        '''Tests that pop returns None and does not break the stack if it
+                is empty'''
+        s = Stack()
+        s.pop()
+        s.pop()
+        s.push(1)
+        s.pop()
+        self.assertTrue(s.pop() is None)
+
+    def test_peek_empty(self):
+        '''Tests that peek on an empty stack returns None'''
+        s = Stack()
+        self.assertTrue(s.peek() is None)
+
 
 if __name__ == '__main__':
     unittest.main()
